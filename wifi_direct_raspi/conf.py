1c03a9f798e256a858568f3aac07831bd3bf4a22
Tatiana Reimer <tatireimer99@gmail.com>
2024-02-19 10:28:43 -0300

    12-p2p-wlan0.network config

--- wifi_direct_raspi/conf.py
+++ wifi_direct_raspi/conf.py
@@ -37,21 +37,71 @@ class Conf:
                 subprocess.run(["sudo", "apt-get", "install", "-y", package])
                 print(f"Package '{package}' installed successfully.")
     '''
-    
+    """
+    Disable processing P2P GO interfaces, by adding this line to the top
+    of the file
+    """
     def disable_dhcpcd():
         with open("/etc/dhcpcd.conf", "a") as f:
             f.write("denyinterfaces p2p-wlan0-*\n")
-    
-    def configure_wpa_supplicant():
+
+    """
+    Configures the wpa_supplicant configuration file, adding P2P settings.
+    If the parameter group_owner=false, the raspberry pi will be a client,
+    is group_owner=true or the parameter is not passed, the raspberry pi 
+    will be the group owner.
+    """
+    def configure_wpa_supplicant(group_owner):
         with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as f:
             f.write("device_name=DIRECT-RasPi1\n")
+            if not group_owner:
+                f.write("p2p_go_intent=0\n")
+            else:
+                f.write("p2p_go_intent=15\n")
             f.write("p2p_go_ht40=1\n")
     
+    """
+    Configures wlan0 P2P GO (group owner) address and systemd-networkd DHCP server
+    """
     def configure_wlan0_p2p_go_address():
-        subprocess.run(["sudo -Es"], check=True)
-        subprocess.run(["cat > /etc/systemd/network/12-p2p-wlan0.network <<\EOF"], check=True)
-        subprocess.run(["sudo -Es"], check=True)
-        subprocess.run(["sudo -Es"], check=True)
-        subprocess.run(["sudo -Es"], check=True)
-        subprocess.run(["sudo -Es"], check=True)
-        subprocess.run(["sudo -Es"], check=True)
+        config = """ 
+        [Match]
+        Name=p2p-wlan0-*
+        [Network]
+        #LLMNR=no
+        #MulticastDNS=yes
+        #IPMasquerade=yes
+        Address=192.168.4.1/24
+        # Comment out the following lines to disable the internal DHCP Server function and use, e.g., dnsmasq
+        DHCPServer=yes
+        [DHCPServer]
+        #DNS=84.200.69.80 1.1.1.1
+        EOF
+        """
+        with open("/etc/systemd/network/12-p2p-wlan0.network", "w") as f:
+            f.write(config)
+
+    """
+    Installs external DHCP server dnsmasq
+    """
+    def install_dnsmasq():
+        subprocess.run(["sudo apt-get install -y dnsmasq"], check=True)
+    
+    def configure_dnsmasq():
+        with open("/etc/dnsmasq.conf", "a") as f:
+            f.write("interface=p2p-wlan*\n")
+            f.write("no-dhcp-interface=lo,wlan0\n")
+            f.write("domain-needed\n")
+            f.write("bogus-priv\n")
+            f.write("dhcp-range=192.168.4.50,192.168.4.199,12h\n")
+            f.write("dhcp-option=3,192.168.50.1\n")
+
+    def enable_and_start_systemd_networkd():
+        subprocess.run(["systemctl enable systemd-networkd.service"], check=True)
+        subprocess.run(["systemctl start systemd-networkd.service"], check=True)
+    
+    """
+    Reboots Raspberry Pi
+    """
+    def reboot():
+        subprocess.run(["sudo reboot"], check=True)
