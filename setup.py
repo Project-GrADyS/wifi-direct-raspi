c130bc279523d5fbae13322c790dedbc1be81e81
Tatiana Reimer <tatireimer99@gmail.com>
2024-04-25 15:18:35 -0300

    setup run

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup
 from setuptools.command.install import install
 import subprocess
 
-
-def run():
-    #install_process = install()
-    #install_process.run()
-    subprocess.run(['sudo', 'python', 'run_conf.py'])
+class InstallConfig(install):
+    def run(self):
+        install.run(self)
+        subprocess.run(['sudo', 'python', 'run_conf.py'])
 
 setup (
     name='wifi_direct_raspi',
-    version='0.0.1',
+    version='0.1',
     description = "Wi-Fi Direct Library for Raspberry Pi",
+    readme = "README.md",
     author="Tatiana Reimer", 
     author_email="tatireimer99@gmail.com",
     url='https://www.python.org/sigs/distutils-sig/',
     cmdclass={
-        'install': run(),
+        'install': InstallConfig,
     },
 )
