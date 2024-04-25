bb9e2d4c075818c8583663f70588211b8f45a01d
Tatiana Reimer <tatireimer99@gmail.com>
2024-04-25 15:28:39 -0300

    setup run

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup
 from setuptools.command.install import install
 import subprocess
@@ -7,6 +8,9 @@ class InstallConfig(install):
         install.run(self)
         subprocess.run(['sudo', 'python', 'run_conf.py'])
 
+def __read__(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
 setup (
     name='wifi_direct_raspi',
     version='0.1',
@@ -14,6 +18,7 @@ setup (
     readme = "README.md",
     author="Tatiana Reimer", 
     author_email="tatireimer99@gmail.com",
+    long_description=__read__('README.md'),
     url='https://www.python.org/sigs/distutils-sig/',
     cmdclass={
         'install': InstallConfig,
